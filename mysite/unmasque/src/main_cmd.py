e9343da226ca486a337f764cfa958ae1ea44a187
Ahana <sendatahana@gmail.com>
2026-09-11 08:16:19 +0530

    Anantha-added rows in customer for y to be 1

--- mysite/unmasque/src/main_cmd.py
+++ mysite/unmasque/src/main_cmd.py
@@ -1338,6 +1338,39 @@ OR (var1_val = 1 AND var2_val = 1 AND var3_val = 1 AND var4_val = 0 AND var5_val
     OR (var10_val = 1 AND var1_val = 1 AND var2_val = 1 AND var3_val = 1)
   );""",
                                False, False, False, False, True),
+                     TestQuery("Customer", """SELECT * 
+FROM customer_buying_record
+WHERE 
+      (x1 = 1 OR y1 = 1)
+  AND (x2 = 1 OR y2 = 1)
+  AND (x3 = 1 OR y3 = 1)
+  AND (x4 = 1 OR y4 = 1)
+  AND (x5 = 1 OR y5 = 1)
+  AND (x6 = 1 OR y6 = 1)
+  AND (x7 = 1 OR y7 = 1)
+  AND (x8 = 1 OR y8 = 1)
+  AND (x9 = 1 OR y9 = 1)
+  AND (x10 = 1 OR y10 = 1)
+  AND (x11 = 1 OR y11 = 1)
+  AND (x12 = 1 OR y12 = 1)
+  AND (x13 = 1 OR y13 = 1)
+  AND (x14 = 1 OR y14 = 1)
+  AND (x15 = 1 OR y15 = 1)
+  AND (x16 = 1 OR y16 = 1)
+  AND (x17 = 1 OR y17 = 1)
+  AND (x18 = 1 OR y18 = 1)
+  AND (x19 = 1 OR y19 = 1)
+  AND (x20 = 1 OR y20 = 1)
+  AND (x21 = 1 OR y21 = 1)
+  AND (x22 = 1 OR y22 = 1)
+  AND (x23 = 1 OR y23 = 1)
+  AND (x24 = 1 OR y24 = 1)
+  AND (x25 = 1 OR y25 = 1)
+  AND (x26 = 1 OR y26 = 1)
+  AND (x27 = 1 OR y27 = 1)
+  AND (x28 = 1 OR y28 = 1)
+  AND (x29 = 1 OR y29 = 1)
+  AND (x30 = 1 OR y30 = 1);""", False, False, False, False, True)
 
                      ]
     return test_workload
@@ -1353,7 +1386,7 @@ if __name__ == '__main__':
 
     # print(workload_dict)
 
-    qid = ("Anantha-Q1") #sys.argv[1]
+    qid = "Customer" #sys.argv[1]
     hq = workload[workload_dict[qid]]
     query = hq.query
     conn = ConnectionHelperFactory().createConnectionHelper()
